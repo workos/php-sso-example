d63599c77d81ea75782b744112a68898ec46fd31
Henry Chan <henry@workos.com>
2020-04-15 22:57:56 -0400

    SSO example

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# php-sso-example
+# python-sso-example
+An example PHP app demonstrating SSO with the [WorkOS PHP SDK](https://github.com/workos-inc/workos-php).
+
+## Dependencies
+Composer - [Link](https://getcomposer.org/)
+
+## Setup
+1. Clone the repo and install the dependencies by running the following:
+    ```bash
+    git clone git@github.com:workos-inc/php-sso-example
+    composer i
+    ```
+1. The example app looks for the following environment variables:
+    - WORKOS_API_KEY - The WorkOS API key can be found [here](https://dashboard.workos.com/api-keys).
+    - WORKOS_PROJECT_ID - The WorkOS Project ID is specific to SSO and can be found [here](https://dashboard.workos.com/sso/configuration)
+
+1. Follow the instructions [here](https://docs.workos.com/sso/auth-flow) on setting up an SSO connection. The redirect URL for the example app if used as is will be http://localhost:8000/auth/callback.
+
+## Running the app
+Use the following command to run the app:
+```bash
+php -S localhost:8000 router.php
+```
+
+Once running, navigate to the following URL for a demonstration on the SSO workflow: http://localhost:8000.
---
+++ router.php
@@ -0,0 +1,59 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Setup html templating library
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+// Configure WorkOS with API Key and Project ID from env
+\WorkOS\WorkOS::setApiKey(getenv("WORKOS_API_KEY"));
+\WorkOS\WorkOS::setProjectId(getenv("WORKOS_PROJECT_ID"));
+
+// Convenient function for throwing a 404
+function httpNotFound() {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404");
+    return true;
+}
+
+// In practice, domain comes from a customer in some way shape or form
+const CUSTOMER_EMAIL_DOMAIN = "foo-corp.com";
+
+// Routing
+switch (strtok($_SERVER["REQUEST_URI"], "?")) {
+    case (preg_match("/\.css$/", $_SERVER["REQUEST_URI"]) ? true: false): 
+        $path = __DIR__ . "/static/css" .$_SERVER["REQUEST_URI"];
+        if (is_file($path)) {
+            header("Content-Type: text/css");
+            readfile($path);
+            return true;
+        }
+        return httpNotFound();
+
+    case ("/auth/callback"):
+        $profile = (new \WorkOS\SSO())->getProfile($_GET["code"]);
+
+        header("Content-Type: application/json");
+        echo json_encode($profile->toArray());
+        return true;
+
+    case ("/"):
+    case ("/login"):
+        echo $twig->render("login.html.twig", [
+            "authUrl" => (new \WorkOS\SSO())
+                ->getAuthorizationUrl(
+                    CUSTOMER_EMAIL_DOMAIN,
+                    'http://localhost:8000/auth/callback',
+                    ["things" => "gonna_my_things_back"],
+                    null
+                )
+            ]
+        );
+        return true;
+
+    default:
+        return httpNotFound();
+}
